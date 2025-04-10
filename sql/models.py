3b2bd67013e52d51cd4589b56585828e8d81251d
wad974 <woodartdeco974@gmail.com>
2025-04-10 09:59:54 +0400

    mise au propre du code dans la class

--- sql/models.py
+++ sql/models.py
@@ -5,16 +5,70 @@ from mysql.connector import errorcode
 # class pour le crud
 class CRUD:
     
-    # on creer un static method
-    @staticmethod
-    async def create( datas : dict ) :
+    def __init__(self):
         
         #on init la base de données
-        login = recupere_connexion_db()
+        self.connexion = recupere_connexion_db()
+    
+    # on creer un static method CREATE
+    @staticmethod
+    async def create(self, datas : dict ) :
+        
         # init cursor
-        cnx  = login.cursor()
+        cnx  = self.connexion
         try:
             #on creer la requete INSERT INTO
+        
+        
+            print('connexion au serveur')
+            return
+        except mysql.connector.Error as err:
+            print('Erreur : ', err)
+    
+            
+    # on creer un static method READ
+    @staticmethod
+    async def read(self, datas : dict ) :
+        
+        # init cursor
+        cnx  = self.connexion
+
+        try:
+            #on creer la requete SELECT
+        
+        
+            print('connexion au serveur')
+            return
+        except mysql.connector.Error as err:
+            print('Erreur : ', err)
+
+    
+    # on creer un static method UPDATE
+    @staticmethod
+    async def update(self, datas : dict ) :
+        
+        # init cursor
+        cnx  = self.connexion
+
+        try:
+            #on creer la requete UPTADE 
+        
+        
+            print('connexion au serveur')
+            return
+        except mysql.connector.Error as err:
+            print('Erreur : ', err)
+            
+    
+    # on creer un static method DELETE
+    @staticmethod
+    async def delete(self, datas : dict ) :
+
+        # init cursor
+        cnx  = self.connexion
+
+        try:
+            #on creer la requete DELETE (SI BESOIN)
         
         
             print('connexion au serveur')
